7128b710707eb0619ace76a957b5b01b669a45d5
MarcoErice <marco.erice@gmail.com>
2017-12-22 10:40:30 +0100

    added Address and Person Models.

---
+++ AddressBook/Models/Address.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Models
+{
+    public class Address
+    {
+        public int AddressID { get; set; }
+        public string Description { get; set; }
+    }
+}
